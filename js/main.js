06378b97e7c235a544d9a27af42cf60f21384c78
Omar Khaled <omar.dev.work@outlook.com>
2021-11-09 20:02:01 +0200

    Fix Scrolling

--- js/main.js
+++ js/main.js
@@ -1,18 +1,27 @@
-
 // Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    let elements = document.getElementsByClassName("nav-link");
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     let elements = document.getElementsByClassName("nav-link");
+
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+//         for (let i = 0; i < elements.length; i++) {
+//             elements[i].classList.remove("active");
+//         }
+//         if (anchor.classList.contains('nav-link')) {
+//             anchor.classList.add('active');
+//         }
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+
+//         });
+//     });
+// });
+window.onload = function () {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
     
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.remove("active");
-        }
-        if (anchor.classList.contains('nav-link')) {
-            anchor.classList.add('active');
-        }
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top - 122
+        }, 500);
     });
-});
+};
